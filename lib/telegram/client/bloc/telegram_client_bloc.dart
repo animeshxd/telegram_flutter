f19061249ad37ae53dab353c094cfe7f73605aaa
Animesh Murmu <am2646374@gmail.com>
2023-09-09 17:59:35 +0530

    Fix initilization of telegram client bloc

--- lib/telegram/client/bloc/telegram_client_bloc.dart
+++ lib/telegram/client/bloc/telegram_client_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tdffi/client.dart';
 import 'package:tdffi/td.dart';
@@ -8,40 +9,64 @@ import 'package:tdffi/td.dart';
 part 'telegram_client_event.dart';
 part 'telegram_client_state.dart';
 
+var logger = Logger('TelegramClientBloc');
+
 class TelegramClientBloc
     extends Bloc<TelegramClientEvent, TelegramClientState> {
   TdlibEventController client;
   final _prefs = SharedPreferences.getInstance();
+  bool initilized = false;
 
   TelegramClientBloc(this.client) : super(const TelegramClientInitial()) {
-    on<ClientInitilizeEvent>((event, emit) async {
-      await client.execute(SetLogVerbosityLevel(new_verbosity_level: 0));
-      debugPrint("Initilizing Client");
-      if (kDebugMode) {
-        var oldId = (await _prefs).getInt('client');
-        if (oldId != null) {
-          client.clientId = oldId;
+    on<InitilizeClient>((event, emit) async {
+      if (!initilized || event.force) {
+        await client.execute(SetLogVerbosityLevel(new_verbosity_level: 0));
+        debugPrint("Initilizing Client");
+        if (kDebugMode) {
+          var oldId = (await _prefs).getInt('client');
+          if (oldId != null) {
+            client.clientId = oldId;
+          }
+          client.sendAsync(Close());
+          // debugPrint(client.clientId.toString());
+          client.clientId = client.td_create_client_id();
+          (await _prefs).setInt('client', client.clientId);
+          // debugPrint(client.clientId.toString());
         }
-        client.sendAsync(Close());
-        debugPrint(client.clientId.toString());
-        client.clientId = client.td_create_client_id();
-        (await _prefs).setInt('client', client.clientId);
-        debugPrint(client.clientId.toString());
+        await client.start();
+        initilized = true;
+        emit(ClientInitilizedState());
       }
-      await client.start();
-      emit(ClientInitilizedState());
     });
   }
 
   @override
   Future<void> close() async {
     await client.destroy();
-    debugPrint("client closed");
+    logger.fine("client closed");
 
     super.close();
   }
+
+  @override
+  void onEvent(TelegramClientEvent event) {
+    super.onEvent(event);
+    logger.fine(event);
+  }
+
+  @override
+  void onChange(Change<TelegramClientState> change) {
+    super.onChange(change);
+    debugPrint(change.currentState.toString());
+  }
 }
 
-final class ClientInitilizeEvent extends TelegramClientEvent {}
+final class InitilizeClient extends TelegramClientEvent {
+  final bool force;
+
+  const InitilizeClient({this.force = false});
+  @override
+  List<Object> get props => [force];
+}
 
 final class ClientInitilizedState extends TelegramClientState {}
