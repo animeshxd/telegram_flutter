8f6626f9f9d6c26380665e593b1ba5923da7b808
Animesh Murmu <am2646374@gmail.com>
2023-09-12 17:12:06 +0530

    add basic profile picture download on loadmessage feature

---
+++ lib/telegram/chat/controller/download_profile_photo.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'dart:io' as io;
+import 'package:flutter/foundation.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:tdffi/client.dart';
+import 'package:tdffi/td.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
+
+class DownloadProfilePhoto {
+  StreamSubscription? _streamSubscription;
+  final TdlibEventController tdlib;
+  Database? _database;
+
+  final state = <int, String>{}.obs;
+
+  Future<Database> get database async => _database ??= await openDatabase(
+        p.join(
+          (await getApplicationCacheDirectory()).path,
+          'file_info_database.db',
+        ),
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute(
+            "CREATE TABLE IF NOT EXISTS file_info (id int primary key, path text not null)",
+          );
+        },
+      );
+
+  DownloadProfilePhoto(this.tdlib) {
+    _streamSubscription = tdlib.updates
+        .whereType<UpdateFile>()
+        .where((event) => event.file.local.is_downloading_completed)
+        .map((event) => event.file)
+        .listen(_updateProfilePhotoIntoDatabase);
+  }
+
+  void downloadFile(File? file) async {
+    debugPrint(file?.local.toString());
+    if (file == null) return;
+    if (file.local.is_downloading_completed ||
+        file.local.is_downloading_active) {
+      return;
+    }
+    await tdlib.send<File>(DownloadFile(
+      file_id: file.id,
+      priority: 1,
+      offset: 0,
+      limit: 0,
+      synchronous: false,
+    ));
+  }
+
+  void _updateProfilePhotoIntoDatabase(File file) async {
+    if (!file.local.is_downloading_completed) return;
+    state[file.id] = file.local.path;
+    state.refresh();
+    var db = await database;
+    await db.insert(
+      'file_info',
+      {'id': file.id, 'path': file.local.path},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  void loadExisting() async {
+    var db = await database;
+
+    var result = (await db.query('file_info'))
+        .map((e) => MapEntry(e['id'] as int, e['path'] as String));
+    state.addEntries(result);
+
+    var list = result
+        .where((f) => !io.File(f.value).existsSync())
+        .map((e) => e.key)
+        .toList();
+    if (list.isEmpty) return;
+    await db.delete(
+      'file_info',
+      where: "id in (${List.filled(list.length, '?').join(',')})",
+      whereArgs: list,
+    );
+  }
+
+  void dispose() async {
+    (await database).close();
+    _database = null;
+    _streamSubscription?.cancel();
+  }
+}
--- lib/telegram/chat/view/chat_screen.dart
+++ lib/telegram/chat/view/chat_screen.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:get/get.dart';
 import 'package:tdffi/client.dart';
 import 'package:tdffi/td.dart' as t;
+import '../controller/download_profile_photo.dart';
 import '../cubit/chat_cubit.dart';
 import '../widget/ellipsis_text.dart';
 import '../../auth/bloc/auth_bloc.dart';
@@ -19,16 +21,20 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   late final TdlibEventController tdlib;
+  late final DownloadProfilePhoto profilePhotoController;
   @override
   void initState() {
     super.initState();
     context.read<ChatCubit>().loadChats();
     tdlib = context.read<TdlibEventController>();
+    profilePhotoController = DownloadProfilePhoto(tdlib); // TODO: move to main.dart
+    profilePhotoController.loadExisting();
   }
 
   @override
   void dispose() {
     super.dispose();
+    profilePhotoController.dispose();
   }
 
   @override
@@ -60,31 +66,14 @@ class _ChatScreenState extends State<ChatScreen> {
                     .toList();
                 chats.sort((a, b) => b.unread_count.compareTo(a.unread_count));
                 return ListView.builder(
+                  addAutomaticKeepAlives: false,
                   itemCount: chats.length,
                   itemBuilder: (context, index) {
                     var chat = chats[index];
-                    var imageSourceb64 = chat.photo?.minithumbnail?.data;
-                    String? title;
-
-                    title = chat.title
-                        .split(" ")
-                        .where((element) => element.isNotEmpty)
-                        .take(2)
-                        .map((e) => e[0])
-                        .join();
-
-                    Widget avatar = CircleAvatar(
-                      child: Text(title),
-                    );
-                    if (imageSourceb64 != null) {
-                      var imageSource = base64.decode(imageSourceb64);
-                      avatar = CircleAvatar(
-                        backgroundImage: MemoryImage(imageSource),
-                      );
-                    }
-
+                    profilePhotoController.downloadFile(chat.photo?.small);
                     return ListTile(
-                      leading: avatar, //TODO: download small photo
+                      //TODO: add better download small photo with retry
+                      leading: leading(chat), 
                       title: EllipsisText(chat.title),
                       subtitle: Align(
                         alignment: Alignment.centerLeft,
@@ -110,6 +99,44 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
     );
+  }
+
+  Widget leading(t.Chat chat) {
+    String title = chat.title
+        .split(" ")
+        .where((element) => element.isNotEmpty)
+        .take(2)
+        .map((e) => e[0])
+        .join();
+    var photo = chat.photo?.small;
+    if (photo == null) {
+      return CircleAvatar(child: Text(title));
+    }
+
+    Widget? avatar = avatarW(photo.local.path);
+    if (avatar != null) return avatar;
+    var imageSourceb64 = chat.photo?.minithumbnail?.data;
+    if (imageSourceb64 != null) {
+      var imageSource = base64.decode(imageSourceb64);
+      avatar = CircleAvatar(backgroundImage: MemoryImage(imageSource));
+    }
+
+    return ObxValue<RxMap<int, String>>(
+      (data) =>
+          avatarW(data[photo.id]) ?? avatar ?? CircleAvatar(child: Text(title)),
+      profilePhotoController.state,
+    );
+  }
+
+  Widget? avatarW(
+    String? path,
+  ) {
+    debugPrint(path);
+    if (path == null) return null;
+    if (path.isEmpty) return null;
+    var file = File(path);
+    if (!file.existsSync()) return null;
+    return CircleAvatar(backgroundImage: FileImage(file));
   }
 
   Widget? subtitle(t.Message? message) {
