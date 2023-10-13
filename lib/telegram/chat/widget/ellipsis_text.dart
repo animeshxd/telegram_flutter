5fac29cc249d32506548624468d25d1af15557a6
Animesh Murmu <am2646374@gmail.com>
2023-10-13 12:48:44 +0530

    Add super.style

--- lib/telegram/chat/widget/ellipsis_text.dart
+++ lib/telegram/chat/widget/ellipsis_text.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class EllipsisText extends Text {
-  const EllipsisText.rich(super.textSpan, {super.key, this.removeNewLine = false});
+  const EllipsisText.rich(super.textSpan, {super.key, this.removeNewLine = false, super.style});
   final bool removeNewLine;
 
-  EllipsisText(String text, {super.key, this.removeNewLine = false})
+  EllipsisText(String text, {super.key, this.removeNewLine = false, super.style})
       : super(
           removeNewLine? text : text.replaceAll('\n', ''),
           maxLines: 1,
