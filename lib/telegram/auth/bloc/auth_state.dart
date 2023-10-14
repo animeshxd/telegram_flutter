2e8e1f11f2b4905a6412c0ea67e1deba50dd3957
Animesh Murmu <am2646374@gmail.com>
2023-10-14 12:22:10 +0530

    Move AuthStateExt to auth_state.dart

--- lib/telegram/auth/bloc/auth_state.dart
+++ lib/telegram/auth/bloc/auth_state.dart
@@ -53,4 +53,34 @@ final class AuthStateTdlibInitilizedFailed extends AuthState {
   const AuthStateTdlibInitilizedFailed(this.tries);
 }
 
-// class AuthCheckCurrentStateRequired extends AuthState {}
+
+extension AuthStateExt on AuthState {
+  /// Automatically do route based on state type
+  void doRoute(BuildContext context) {
+    if (this is AuthStateTdlibInitilizedFailed) {
+      context.replace(TdlibInitFailedPage.path, extra: this);
+    }
+    if (this is AuthStateLoginRequired) {
+      context.replace('/login');
+    }
+    if (this is AuthStateCodeRequired) {
+      var codeInfo = (this as AuthStateCodeRequired).codeInfo;
+      context.replace('/login/otp', extra: codeInfo);
+    }
+    if (this is AuthStateCodeInvalid) {
+      context.replace('/login/otp#invalid');
+    }
+    if (this is AuthStatePhoneNumberOrBotTokenRequired) {
+      context.replace('/login/phoneNumber');
+    }
+    if (this is AuthStatePhoneNumberInvalid) {
+      context.replace('/login/phoneNumber#invalid');
+    }
+    if (this is AuthStateBotTokenInvalid) {
+      context.replace("/login/botToken#invalid");
+    }
+    if (this is AuthStateCurrentAccountReady) {
+      context.replace(ChatListScreen.path, extra: this);
+    }
+  }
+}
